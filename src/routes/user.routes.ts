0a4b2869ce9f359cc3b623bbfbe91d3415607d1d
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-22 22:37:05 -0300

    update routes, added get and put

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,7 +3,10 @@ import * as userController from '../controllers/user.controller';
 
 const router = express.Router();
 
+//CRUD
 router.post('/', userController.createUser)
+router.get('/', userController.getUsers)
+router.put('/:id', userController.editUser)
 router.delete('/:id', userController.deleteUser)
 
 export default router;
