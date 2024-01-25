d0b4f1ee561a18def46f7a55766cd20e5911c2a1
kennedysmartins <kennedysmartins@hotmail.com>
2024-01-25 12:43:24 -0300

    update router createDefaultConfig

--- src/routes/store.routes.ts
+++ src/routes/store.routes.ts
@@ -5,7 +5,7 @@ import * as storeController from '../controllers/store.controller';
 const router = express.Router()
 
 router.get('/', storeController.getAllConfigs)
-router.post('/', storeController.createDefaultConfig)
+router.post('/createDefaultConfig', storeController.createDefaultConfig)
 router.put('/:id', storeController.editConfig);
 
 
